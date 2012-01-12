9eca79775479cd094e2b7eff95c6df07862dd966
Yutong Wang <hybridthesis@gmail.com>
2012-01-12 16:44:42 -0500

    Use stuff

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
-#To install do this
+#Create symlink
+
+ln -s ./vimrc ~/.vimrc
+
+#To install do this, get pathogen
+
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
 curl -so ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
-
